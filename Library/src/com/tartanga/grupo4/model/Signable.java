33cc6f05f578120cb7868bab41b2b2ac68d05a64
irodrigon <irodrigon2313@gmail.com>
2024-10-18 09:21:46 +0200

    added connection pool and empty properties connection file

---
+++ Library/src/com/tartanga/grupo4/model/Signable.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tartanga.grupo4.model;
+
+/**
+ *
+ * @author rabio
+ */
+public interface Signable {
+    
+    public User signIn(User user);
+    public User signUp(User user);
+}
--- Library/src/com/tartanga/grupo4/model/User.java
+++ Library/src/com/tartanga/grupo4/model/User.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Example;
+package com.tartanga.grupo4.model;
 
 /**
  *
- * @author Iñi
+ * @author rabio
  */
-public class Example {
+public class User {
     
 }
